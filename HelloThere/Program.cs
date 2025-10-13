7c93de497bb02b6032b0b545ded530a23fab047c
Raul Smith <raul.n.smith@gmail.com>
2025-10-13 15:40:16 -0700

    Initial repo - Learning basics of C#

---
+++ HelloThere/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Who are you? ");
+        string name = Console.ReadLine();
+
+        Console.Write("How old are you? ");
+        string age = Console.ReadLine();
+
+        Console.WriteLine($"Hello, {name}, you are {age} years old!");
+    }
+}
